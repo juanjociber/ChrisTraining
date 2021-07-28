16dc9810e3d090cbc0d866d617449b65b0a03b55
unknown <i202015638@cibertec.edu.pe>
2021-07-27 19:58:49 -0500

    Estilo banner y header

--- js/eventos.js
+++ js/eventos.js
@@ -26,3 +26,79 @@ function MenuResponsive(){
         }
     })
 };
+
+/**================================================
+ * CONTROL DE SUB_MENÚ RESPONSIVE
+ * ================================================
+ */
+const icoAngulo = document.querySelector('#icoAngulo')
+const subMenu = document.querySelector('.submen')
+let mqMax767 = window.matchMedia('(max-width:767px)')
+conta = 1    
+
+function mqSubmenu(mqMax767){
+    if(mqMax767.matches){
+        console.log('movil')
+        subMenu.style.display ='none'
+        
+    }
+    else{
+        console.log('desktop')
+        subMenu.style.display='block'
+        
+    }
+}
+mqSubmenu(mqMax767)
+mqMax767.addListener(mqSubmenu)
+
+icoAngulo.addEventListener('click',function(e){
+    if(conta == 1){
+        subMenu.style.display='block'
+        icoAngulo.setAttribute('transform','rotate(180)')
+        conta = 0
+    }
+    else{
+        conta = 1
+        subMenu.style.display='none'
+        icoAngulo.setAttribute('transform','rotate(0)')
+    }
+})
+
+/**=======================================================
+ *   TEXTO PARALLAX (ESCRIBE CARACTER POR CARACTER)
+ =======================================================*/
+function viewAqui(){
+   const $click = document.querySelector('#click')
+   let printCadena
+   const observando= new IntersectionObserver(function(e){
+       if(e[0].isIntersecting){
+           console.log('Ingreso de evento');
+           let escritura = str =>{
+                let arrayStr = str.split('');
+                let i = 0;
+                printCadena = setInterval(function () {
+                    if(arrayStr[i] === ''){
+                        $click.innerHTML += arrayStr[i];
+                        $click.innerHTML += arrayStr[i + 1];
+                        i +=2;                        
+                    }
+                    else{
+                        $click.innerHTML += arrayStr[i];
+                        i++
+                    }
+                    if(i=== arrayStr.length){
+                        clearInterval(printCadena);
+                    }
+                },300);
+           }
+           escritura('Aquí');
+       }
+       else{
+           console.log('Salida de evento')
+           clearInterval(printCadena)
+           $click.innerHTML= ""
+       }
+   });
+   observando.observe(document.querySelector('#banner'))
+};
+viewAqui();
