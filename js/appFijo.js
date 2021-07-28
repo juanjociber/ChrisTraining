f5dd51077246c931f1a58178ae1a9852926ba289
unknown <i202015638@cibertec.edu.pe>
2021-07-28 09:18:04 -0500

    Estilo menú

---
+++ js/appFijo.js
@@ -0,0 +1,21 @@
+/**=======================================================
+ *              NAVEGACIÓN ESTÁTICA
+ =========================================================*/
+ document.addEventListener('DOMContentLoaded', function() {
+    navegacionFija();
+});
+function navegacionFija() {
+    const barra1 = document.querySelector('#nav');
+    // Registrar el Intersection Observer
+    const observer = new IntersectionObserver( function(entries) {
+        if(entries[0].isIntersecting) {
+            barra1.classList.remove('fijo');
+        } else {
+            barra1.classList.add('fijo');
+        }
+    });
+    // Elemento a observar
+    observer.observe(document.querySelector('#fijo'));
+};
+
+
