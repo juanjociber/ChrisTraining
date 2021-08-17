bc094eb8cd509c15a649fa8f2b781e9501ba5755
unknown <i202015638@cibertec.edu.pe>
2021-08-16 20:15:08 -0500

    Terminando estilos a sliders

--- js/carousel-index.js
+++ js/carousel-index.js
@@ -12,24 +12,65 @@ const imagenesCarousel = [{image:'img/1.jpg',perfil:'perfil1'},
 
 let imgArray = Object.entries(imagenesCarousel);
 
-const $Carousel__lista = document.querySelector('.carousel__lista')
-const $fragmentCarousel = document.createDocumentFragment();
+// const $Carousel__lista = document.querySelector('.carousel__lista')
+// const $fragmentCarousel = document.createDocumentFragment();
 
-imgArray.forEach(function(event){
-    // console.log(event[1].image)
-    const $liCarousel = document.createElement("LI")
-    const $imgCarousel = document.createElement("IMG")
-    const $pCarousel = document.createElement('P')
+// imgArray.forEach(function(event){
+//     // console.log(event[1].image)
+//     const $liCarousel = document.createElement("LI")
+//     const $imgCarousel = document.createElement("IMG")
+//     const $pCarousel = document.createElement('P')
 
-    $liCarousel.classList.add("carousel__elemento")
-    $imgCarousel.setAttribute("src",event[1].image)
-    $imgCarousel.setAttribute("Alt","imagen")
-    $pCarousel.textContent=event[1].perfil
-    $pCarousel.classList.add('center-aling')
+//     $liCarousel.classList.add("carousel__elemento")
+//     $imgCarousel.setAttribute("src",event[1].image)
+//     $imgCarousel.setAttribute("Alt","imagen")
+//     $pCarousel.textContent=event[1].perfil
+//     $pCarousel.classList.add('center-aling')
 
-    $liCarousel.appendChild($imgCarousel)
-    $liCarousel.appendChild($pCarousel)
+//     $liCarousel.appendChild($imgCarousel)
+//     $liCarousel.appendChild($pCarousel)
     
-    $fragmentCarousel.appendChild($liCarousel)
-    $Carousel__lista.appendChild($fragmentCarousel)
-});
+//     $fragmentCarousel.appendChild($liCarousel)
+//     $Carousel__lista.appendChild($fragmentCarousel)
+// });
+
+const $caruseles = document.querySelector('#caruseles');
+let $carusel = document.querySelectorAll('.carusel');
+let $caruselLast = $carusel[$carusel.length - 1];
+
+const $btn_Left = document.querySelector('#btnCarusel-left');
+const $btn_Rigth = document.querySelector('#btnCarusel-right');
+
+dispositivo576 = ()=> window.innerWidth >=576; 
+
+$caruseles.insertAdjacentElement('afterbegin',$caruselLast);
+
+function siguiente(){
+    let $caruselFirst = document.querySelectorAll('.carusel')[0];
+    $caruseles.style.marginLeft = '-100%';
+    $caruseles.style.transition ='all 0.5s';
+    setTimeout(() => {
+        $caruseles.style.transition = 'none';
+        $caruseles.insertAdjacentElement('beforeend',$caruselFirst);
+        $caruseles.style.marginLeft = '-100%';
+    }, 500);
+}
+$btn_Rigth.addEventListener('click',function(){
+    siguiente();
+})
+
+function atras(){
+    let $carusel = document.querySelectorAll('.carusel');
+    let $caruselLast = $carusel[$carusel.length - 1];
+
+    $caruseles.style.marginLeft = '0';
+    $caruseles.style.transition ='all 0.5s';
+    setTimeout(() => {
+        $caruseles.style.transition = 'none';
+        $caruseles.insertAdjacentElement('afterbegin',$caruselLast);
+        $caruseles.style.marginLeft = '-100%';
+    }, 500);
+}
+$btn_Left.addEventListener('click',function(){
+    atras();
+})
