bed6e8fb5c1151030dc37da57fac7480ea91e932
unknown <i202015638@cibertec.edu.pe>
2021-08-16 22:02:18 -0500

    slider desarrollado en js

--- js/carousel-index.js
+++ js/carousel-index.js
@@ -1,39 +1,68 @@
 /*===================================
 -> CREANDO CAROUSEL:@Christraining
 =====================================*/
-const imagenesCarousel = [{image:'img/1.jpg',perfil:'perfil1'},
-                          {image:'img/2.jpg',perfil:'perfil2'},
-                          {image:'img/3.jpg',perfil:'perfil3'},
-                          {image:'img/4.jpg',perfil:'perfil4'},
-                          {image:'img/5.jpg',perfil:'perfil5'},
-                          {image:'img/3.jpg',perfil:'perfil6'},
-                          {image:'img/5.jpg',perfil:'perfil7'},
-                          {image:'img/4.jpg',perfil:'perfil8'}]
+const imagenesCarousel = [{image:'img/1.jpg',perfil:'perfil 1'},
+                          {image:'img/2.jpg',perfil:'perfil 2'},
+                          {image:'img/3.jpg',perfil:'perfil 3'},
+                          {image:'img/4.jpg',perfil:'perfil 4'},
+                          {image:'img/5.jpg',perfil:'perfil 5'},
+                          {image:'img/3.jpg',perfil:'perfil 6'},
+                          {image:'img/5.jpg',perfil:'perfil 7'},
+                          {image:'img/4.jpg',perfil:'perfil 8'}]
 
 let imgArray = Object.entries(imagenesCarousel);
 
-// const $Carousel__lista = document.querySelector('.carousel__lista')
-// const $fragmentCarousel = document.createDocumentFragment();
+const $ulCarusel = document.querySelector('.carusel-contenedor')
+const $divCarusel = document.createElement('div')
+const $divRigth = document.createElement('div')
+const $divLeft = document.createElement('div')
+$divRigth.classList.add('carusel__btn')
+$divRigth.classList.add('carusel__btn--rigth')
+$divRigth.id='btnCarusel-right'
+$divRigth.innerHTML= 
+`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right"
+    width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none"
+    stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <polyline points="9 6 15 12 9 18" />
+</svg>`
+$divLeft.classList.add('carusel__btn')
+$divLeft.classList.add('carusel__btn--left')
+$divLeft.id='btnCarusel-left'
+$divLeft.innerHTML=`
+<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="44"
+    height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none"
+    stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <polyline points="15 6 9 12 15 18" />
+</svg>`
+$divCarusel.classList.add('caruseles')
+$divCarusel.id='caruseles'
+$ulCarusel.appendChild($divCarusel);
 
-// imgArray.forEach(function(event){
-//     // console.log(event[1].image)
-//     const $liCarousel = document.createElement("LI")
-//     const $imgCarousel = document.createElement("IMG")
-//     const $pCarousel = document.createElement('P')
+imgArray.forEach(function (e) {
+    // console.log(e[1])
+    const $liCarusel = document.createElement('LI')
+    const $imgCarusel = document.createElement('IMG')
+    const $pCarusel = document.createElement('P')
 
-//     $liCarousel.classList.add("carousel__elemento")
-//     $imgCarousel.setAttribute("src",event[1].image)
-//     $imgCarousel.setAttribute("Alt","imagen")
-//     $pCarousel.textContent=event[1].perfil
-//     $pCarousel.classList.add('center-aling')
+    $liCarusel.classList.add('carusel')
+    $imgCarusel.setAttribute('src',e[1].image)
+    $imgCarusel.setAttribute('Alt','imagen')
+    $pCarusel.innerHTML=e[1].perfil
+        
+    $liCarusel.appendChild($imgCarusel)
+    $liCarusel.appendChild($pCarusel)
+    $divCarusel.appendChild($liCarusel)
+});
 
-//     $liCarousel.appendChild($imgCarousel)
-//     $liCarousel.appendChild($pCarousel)
-    
-//     $fragmentCarousel.appendChild($liCarousel)
-//     $Carousel__lista.appendChild($fragmentCarousel)
-// });
+$divCarusel.appendChild($divRigth)
+$divCarusel.appendChild($divLeft)
 
+
+/**===========================
+ * -> EVENTO DEL CARUSEL
+ =============================*/
 const $caruseles = document.querySelector('#caruseles');
 let $carusel = document.querySelectorAll('.carusel');
 let $caruselLast = $carusel[$carusel.length - 1];
