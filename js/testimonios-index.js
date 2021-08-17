29b38cecfbd561faa870221b6d771eed028ae994
unknown <i202015638@cibertec.edu.pe>
2021-08-17 10:19:31 -0500

    empezando página entrenamiento

--- js/testimonios-index.js
+++ js/testimonios-index.js
@@ -3,9 +3,9 @@
 =====================================*/
 const testimonios=[
     {   imgTestimonio:'img/persona1.jpg' ,
-        comentario: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum voluptas est veniam. Neque
+        comentario: `<span class="comillas">"</span> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum voluptas est veniam. Neque
                       rem atque dolore ratione amet nam, fugiat, tempora et necessitatibus consequatur odit? Et maiores
-                      deserunt illum molestiae.`,
+                      deserunt illum molestiae. <span class="comillas">"</span>`,
         nombre: 'Vania H.F.',
         distrito: 'Los Olivos'},
     {   imgTestimonio:'img/persona2.jpg' ,
@@ -20,10 +20,35 @@ let testimonioArray = Object.entries(testimonios);
 
 const $containerTestimonio = document.querySelector('.container-sliderTestimonio')
 const $sliderDiv = document.createElement('DIV')
+const $sliderTestBtnRigth = document.createElement('DIV')
+const $sliderTestBtnLeft = document.createElement('DIV')
+
+$sliderTestBtnRigth.classList.add('sliderTestimonio__btn')
+$sliderTestBtnRigth.classList.add('sliderTestimonio__btn--rigth')
+$sliderTestBtnRigth.id='btnTestimonio-right'
+$sliderTestBtnRigth.innerHTML=
+`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right"
+    width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#bbbbbbe1" fill="none"
+    stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <polyline points="9 6 15 12 9 18" />
+</svg>`
+$sliderTestBtnLeft.classList.add('sliderTestimonio__btn')
+$sliderTestBtnLeft.classList.add('sliderTestimonio__btn--left')
+$sliderTestBtnLeft.id='btnTestimonio-left'
+$sliderTestBtnLeft.innerHTML=
+` <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="44"
+    height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#bbbbbbe1" fill="none"
+    stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <polyline points="15 6 9 12 15 18" />
+</svg>`
 $sliderDiv.classList.add('sliderTestimonio')
 $sliderDiv.id='sliderTestimonio'
 
 $containerTestimonio.appendChild($sliderDiv)
+$containerTestimonio.appendChild($sliderTestBtnRigth)
+$containerTestimonio.appendChild($sliderTestBtnLeft)
 
 testimonioArray.forEach(function(e){
     console.log(e[1])
@@ -32,44 +57,27 @@ testimonioArray.forEach(function(e){
     const $sliderContenido = document.createElement('DIV')
     const $psliderComentario = document.createElement('P')
     const $psliderNombre = document.createElement('P')
-    const $spanComillasInicio = document.createElement('SPAN')
-    const $spanComillasFin = document.createElement('SPAN')
-    
     const $smallDistrito = document.createElement('SMALL')
 
     $smallDistrito.textContent=e[1].distrito
     $smallDistrito.classList.add('distrito')
-    $spanComillasInicio.classList.add('comillas')
-    $spanComillasInicio.textContent='"'
-    $spanComillasFin.classList.add('comillas')
-    $spanComillasFin.textContent='"'
     $psliderNombre.textContent=e[1].nombre
     $psliderNombre.classList.add('nombre')
     $psliderComentario.classList.add('comentario')
-    $psliderComentario.textContent=e[1].comentario
+    $psliderComentario.innerHTML =e[1].comentario
     $sliderSection.classList.add('sliderTestimonio__section')
     $imgSlider.classList.add('sliderTestimonio__img')
     $imgSlider.setAttribute('src',e[1].imgTestimonio)
     $imgSlider.setAttribute('Alt','imagenTestimonio')
     $sliderContenido.classList.add('sliderTestimonio__contenido')
-
-
-    $psliderComentario.appendChild($spanComillasInicio)
-    // $psliderComentario.append($spanComillasFin)
-    
-    
-    
+      
     $sliderContenido.appendChild($psliderComentario)
     $sliderContenido.appendChild($psliderNombre)
     $sliderContenido.appendChild($smallDistrito)
-
-    
     $sliderSection.appendChild($imgSlider)
     $sliderSection.appendChild($sliderContenido)
     $sliderDiv.appendChild($sliderSection)
 });
-
-
 
 /**======================================
  * -> EVENTO SLIDER TESTIMONIO
@@ -115,7 +123,3 @@ $btnLeft.addEventListener('click',function(){
     Prev();
 })
 
-
-// setInterval(() => {
-//    Next(); 
-// }, 10000);
