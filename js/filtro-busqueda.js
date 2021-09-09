994421de34892a6a4202a4a47ef9aaade6dfac4f
unknown <i202015638@cibertec.edu.pe>
2021-09-08 19:30:05 -0500

    buscador terminado

--- js/filtro-busqueda.js
+++ js/filtro-busqueda.js
@@ -1,4 +1,8 @@
+/**==================================
+ * Evento de elemento 'input' buscar
+ ====================================*/
 $(document).ready(function(){
+    esDispositivoMovil = ()=> window.innerWidth >=768; 
     var busqueda = $('#busqueda'),
     titulo = $('main section h2');
     $(titulo).each(function(){
@@ -16,6 +20,10 @@ $(document).ready(function(){
                 $(clase).css('margin-top', '1px');
             }else{
                 $(clase).attr('class', 'bx bx-search');
+                $(clase).css('margin-top', '2px');
+                if(esDispositivoMovil()){
+                    $(clase).css('margin-top', '0px');
+                }
             }
             if($(clase).hasClass('bx bx-eraser')){
                 $(clase).click(function(){
@@ -40,36 +48,43 @@ $(document).ready(function(){
     });
 });
 
-const $input = document.querySelector('#busqueda')
-const $lupa = document.querySelector('#lupa')
-const $search = document.querySelector('.search')
-const $cerrarLupa = document.querySelector('#cerrarLupa')
-
-$lupa.addEventListener('click',function(e){
-    console.log(e.target);
-    contador = 1;
-    if(contador == 1){
-        $input.style='visibility:visible;width:100%;transition:all .6s;outline: unset;border-radius:10px'
-        $lupa.style='right: 40px; color:#b5b5b5; text-shadow:none; top:10px; font-size:22px'
-        $search.style='left:10%; transition:.5s'
-        $cerrarLupa.style='visibility:visible'
-        contador = 0
-    }else{
-        contador = 1
-    }
+document.addEventListener('DOMContentLoaded',function(){
+    eventoSearch();
 })
+function eventoSearch(){
+    const $input = document.querySelector('#busqueda')
+    const $lupa = document.querySelector('#lupa')
+    const $search = document.querySelector('.search')
+    const $cerrarLupa = document.querySelector('#cerrarLupa')
 
-$cerrarLupa.addEventListener('click',function(){
-    if(contador == 0){
-        $input.style='visibility:hidden; transition:all .1s'
-        $input.innerHTML=''
-        $cerrarLupa.style='visibility:hidden'
-        $search.style='left:18%; transition:.5s'
-        $lupa.setAttribute('class','bx bx-search')
-        $lupa.style='color:white'
-        contador = 1
-    }
-    else{
-        contador = 0
-    }
-})
+    $lupa.addEventListener('click',function(e){
+        console.log(e.target);
+        contador = 1;
+        if(contador == 1){
+            $input.style='visibility:visible;width:100%;transition:all .6s;outline: unset;border-radius:10px'
+            $lupa.style='right: 40px; color:#b5b5b5; text-shadow:none; top:10px; font-size:22px'
+            $search.style='width:80%; transition:.5s; transform:translateX(0)'
+            $cerrarLupa.style='visibility:visible'
+            contador = 0
+        }
+        else{
+            contador = 1
+        }
+    })
+
+    $cerrarLupa.addEventListener('click',function(){
+        if(contador == 0 && $input.value==''){
+            $input.style='visibility:hidden; transition:all .1s'
+            $input.innerHTML=''
+            $cerrarLupa.style='visibility:hidden'
+            $search.style='left:10%; transition:.5s'
+            // $lupa.setAttribute('class','bx bx-search')
+            $lupa.style='color:white'
+            contador = 1
+        }
+        else{
+            contador = 0
+        }
+    })
+};
+
