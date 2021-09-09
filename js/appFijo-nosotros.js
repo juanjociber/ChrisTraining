514aea145646062924d65b2a8543bdf4f5ae2c82
unknown <i202015638@cibertec.edu.pe>
2021-09-08 23:02:02 -0500

    agregando buscador

--- js/appFijo-nosotros.js
+++ js/appFijo-nosotros.js
@@ -3,12 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 function navegacionFijaNosotros() {
     const barra = document.querySelector('.nav');
+    const $search = document.querySelector('#buscador')
     // Registrar el Intersection Observer
     const observer = new IntersectionObserver( function(entries) {
         if(entries[0].isIntersecting) {
             barra.classList.remove('fijo');
+            $search.classList.remove('fijoBuscador')
         } else {
             barra.classList.add('fijo');
+            $search.classList.add('fijoBuscador')
         }
     });
     // Elemento a observar
